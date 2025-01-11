00483bb53f27386a3a989a7baa5744de28422524
ahsanch116 <ahsanch116@gmail.com>
2025-01-11 12:55:52 +0100

    Updating Unit test

--- Semantic_Analysis/Semantic_Analysis/obj/Debug/net8.0/Semantic_Analysis.AssemblyInfo.cs
+++ Semantic_Analysis/Semantic_Analysis/obj/Debug/net8.0/Semantic_Analysis.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Semantic_Analysis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+350a36aaa68168560ed9144e098070358f93bb71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd7dfe6c100ea34f8ab64eba3e22cf5d7b888571")]
 [assembly: System.Reflection.AssemblyProductAttribute("Semantic_Analysis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Semantic_Analysis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
