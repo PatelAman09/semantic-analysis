70c0d52e55adc1b83a75aac0c7c00a20648d0b4a
Aman Patel <amanpatel60439@gmail.com>
2025-01-11 15:24:58 +0100

    Updating File

--- Semantic_Analysis/UnitTestProject/obj/Debug/net8.0-windows/UnitTestProject.AssemblyInfo.cs
+++ Semantic_Analysis/UnitTestProject/obj/Debug/net8.0-windows/UnitTestProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f183da27c218b45a7991b410e622c397245eb083")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e6f1df956ac32f4a11afe30e22610ae80e36abf")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
