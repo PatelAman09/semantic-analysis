9f37c7faee945e9df777b11e13a9b6cb0ea80a6d
Aman Patel <amanpatel60439@gmail.com>
2025-01-15 18:26:00 +0100

    Updating Git Ignore File

--- Semantic_Analysis/Semantic_Analysis/obj/Debug/net8.0/Semantic_Analysis.AssemblyInfo.cs
+++ Semantic_Analysis/Semantic_Analysis/obj/Debug/net8.0/Semantic_Analysis.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Semantic_Analysis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1820942e018654d703147cf7a750c0f0e981588")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+956aa23831916a26f1fb533a10264bf729c0b57c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Semantic_Analysis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Semantic_Analysis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
