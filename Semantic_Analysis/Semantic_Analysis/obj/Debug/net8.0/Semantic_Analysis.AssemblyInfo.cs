20033fd631ff41c59ead56d373b005f399db7304
ahsanch116 <ahsanch116@gmail.com>
2025-01-21 23:13:33 +0100

    Working with embedding test functions

--- Semantic_Analysis/Semantic_Analysis/obj/Debug/net8.0/Semantic_Analysis.AssemblyInfo.cs
+++ Semantic_Analysis/Semantic_Analysis/obj/Debug/net8.0/Semantic_Analysis.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Semantic_Analysis")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+78afddd311d64a8c7d536902759d397cf1f678fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0080e944d4105c516a27684d22cec17e5e5933f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Semantic_Analysis")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Semantic_Analysis")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
