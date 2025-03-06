5c2e838837d05dd65e4459a8b21243c6f5836ff6
Lenovo <saquibattar04@gmail.com>
2025-03-06 18:35:10 +0530

    Updated DataExtraction code to store the outputs in one folder

--- Semantic_Analysis/UnitTestProject/obj/Debug/net8.0-windows/UnitTestProject.AssemblyInfo.cs
+++ Semantic_Analysis/UnitTestProject/obj/Debug/net8.0-windows/UnitTestProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dcdc7497df862ac727540e0bd86f8408b17487c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb6887075aa070c5079b1db1e96abf710a4a7394")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
